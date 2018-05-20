e73dad6b11df1cfba3eaa24fce26dfec615738b9
wangqi880 <444687196@qq.com>
2018-05-20 23:07:05 +0800

    简单集成redis

---
+++ src/test/java/com/j/RedisTest.java
@@ -0,0 +1,29 @@
+package com.j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @version 1.0 created by xxxx on 2018/5/20 23:00
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    public void test(){
+        stringRedisTemplate.opsForValue().set("name","wq");
+        String name = stringRedisTemplate.opsForValue().get("name");
+        System.out.println(name);
+    }
+}
